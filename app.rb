f8c13cd4e8ca63d4916310e846ad9f717e568dac
ArvidMarild <152278740+ArvidMarild@users.noreply.github.com>
2025-01-27 14:01:24 +0100

    början till fungerande singup

--- app.rb
+++ app.rb
@@ -4,4 +4,40 @@ class App < Sinatra::Base
         erb(:"index")
     end
 
+    def db
+      return @db if @db
+
+      @db = SQLite3::Database.new("db/beatsun.sqlite")
+      @db.results_as_hash = true
+
+      return @db
+    end
+
+    get '/loginlink' do
+        erb :login
+      end
+
+    post '/login' do
+
+    end
+      
+    get '/signuplink' do
+      erb :signup
+    end
+
+    post '/singup' do
+      puts "PARAMS: #{params.inspect}" # Log parameters for debugging
+
+      if params["signup-password"] == params["signup-confirm-password"]
+        db.execute("INSERT INTO users (email, username, password) VALUES(?,?,?)", 
+        [   
+            params["signup-email"],
+            params["signup-name"],
+            params["signup-password"]
+        ])
+        redirect "/"
+      else
+        erb :signupreject
+      end
+    end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,9 +1,39 @@
 require 'sqlite3'
+require 'bcrypt'
 
 class Seeder
 
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS users')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL)')
+  end
+
+  def self.populate_tables
+    password_hashed = BCrypt::Password.create('arvid12345')
+    p "Storing hashed version of password to db. Clear text never saved. #{password_hashed}"
+    db.execute('INSERT INTO users (email, username, password) VALUES (?, ?, ?)', ['arvid.marild@gmail.com', 'arvid', password_hashed])
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/beatsun.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
+
+Seeder.seed!
